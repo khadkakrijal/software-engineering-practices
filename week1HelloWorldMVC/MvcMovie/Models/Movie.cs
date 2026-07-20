56abb9d2e44bc0172d8f2ced272f4cb6f05af896
khadkakrijal <khadkakrijal36@gmail.com>
2026-07-20 13:33:45 +0930

    Add movie model validation

--- week1HelloWorldMVC/MvcMovie/Models/Movie.cs
+++ week1HelloWorldMVC/MvcMovie/Models/Movie.cs
@@ -8,17 +8,33 @@ namespace MvcMovie.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(
+            @"^[A-Z]+[a-zA-Z\s]*$",
+            ErrorMessage = "Genre must start with a capital letter and contain only letters and spaces."
+        )]
         public string Genre { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
+        [Required]
+        [StringLength(5)]
+        [RegularExpression(
+            @"^[A-Z]+[a-zA-Z0-9""'\s-]*$",
+            ErrorMessage = "Rating must start with a capital letter and can contain letters, numbers, spaces, apostrophes, quotes, or hyphens."
+        )]
         public string Rating { get; set; }
     }
 }
